82bf8d6be1b347368ce374e20c0278ca8c07c681
Zachary P <zack@zackporter.com>
2020-02-14 18:18:50 -0500

    ds

--- project2/README.txt
+++ project2/README.txt
@@ -1,1 +1,24 @@
-fdsfsdfdsfsdfsdfsd
+------------
+Project 2
+Zachary Porter
+------------
+
+This is my work for project 2. All code is contained in main.c and is compiled to the file "memorymanager". Accepted commands: map, store, read. 
+
+Everything was build as per the specifications provided by Professor Walls with the exception of exact output formatting
+
+Example input:
+    testInput.txt
+Example output:
+    exampleOutput.txt
+
+Eviction strategy:
+    -- Dont evict if pages are already in
+    -- Dont evict if table is already in and there is enough space for another page
+    -- Evict pages of process if there is not enough space for another  
+    -- Evict pages if there is not enough space for new table and new page
+
+RESOURCES CONSULTED:
+https://www.rapidtables.com/convert/number/decimal-to-binary.html
+https://ia.wpi.edu/cs3013-walls/resources.php?page=show_project&id=4
+https://www.programiz.com/c-programming/c-file-input-output
--- project2/main.c
+++ project2/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #define PROCESS_ID 0
 #define INSTRUCTION_TYPE 1
 #define VIRTUAL_ADDRESS 2
@@ -15,6 +16,7 @@ int32_t claimedPages[4];
 char input[4][32];
 
 FILE *file;
+int swapout(int pid);
 int getNextPage(){
     for (int i=0; i<4; i++){
         if (!claimedPages[i]){
@@ -47,9 +49,25 @@ void sflush(){
     }
 }
 void swapin(int pid, int VPN){
+    bool notableevict=false;
+    if (pidPageTables[pid]!=-1){
+        // is present
+        if ((memory[pidPageTables[pid]+VPN] & 0x20)){
+            printf("Page in memory. No swapping \n");
+            return;
+        }
+        if (freePages()<1){
+            swapout(pid);
+        }else{
+            printf("No table evict\n");
+            notableevict=true;
+        }
+    }
+ //   sflush();
     printf("Swapping in %d & %d\n",pid*5,pid*5+VPN+1);
     
-    if (freePages()<2){
+    if (!notableevict && freePages()<2){
+        printf("Free pages: %d. Flushing\n",freePages());
         sflush();
     }   
     file=fopen("swap","r");
@@ -61,29 +79,34 @@ void swapin(int pid, int VPN){
     for (int i=0; i<VPN+1; i++){
         fread(&tmpPG, 16, 1, file);
     }
-    int emptyPageLoc = getNextPage()*16;
-    claimPage(emptyPageLoc/16);
+    int emptyPageLoc =0;;
+    if (notableevict){
+        emptyPageLoc = pidPageTables[pid];
+    }else{
+        emptyPageLoc = getNextPage()*16;
+        claimPage(emptyPageLoc/16);
+    }
     int emptyPage2Loc = getNextPage()*16;
     claimPage(emptyPage2Loc/16);
         pidPageTables[pid]=emptyPageLoc;
-        printf("Claiming physical page %d for map\n",emptyPageLoc);
+        printf("Claiming physical page %d for map\n",emptyPageLoc/16);
     if (pidPageTables[pid]==-1){
     }
     fclose(file);
     for (int x=0; x<16; x++){
-        memory[emptyPageLoc+x] = tmpTBL[x];
-    }
-    for (int x=0; x<16; x++){
         memory[emptyPage2Loc+x] = tmpPG[x];
     }
-    
-    for (int x=0; x<4; x++){
-        memory[emptyPageLoc+x] &= 0xC0; // clear  the vpn and present
+    if (!notableevict){
+        for (int x=0; x<16; x++){
+            memory[emptyPageLoc+x] = tmpTBL[x];
+        }
+        for (int x=0; x<4; x++){
+            memory[emptyPageLoc+x] &= 0xC0; // clear  the vpn and present
+        }
     }
     // present in memory
     SET_BIT(memory[pidPageTables[pid]+VPN],5);
     memory[emptyPageLoc+VPN] |= emptyPage2Loc/16;
-    printf("MAP VAL:%d other %d\n",memory[emptyPageLoc+VPN],emptyPage2Loc/16);
     printf("Swapped in\n");
 }
 void swapclean(int frame, int swapFrame){
@@ -92,7 +115,6 @@ void swapclean(int frame, int swapFrame){
     fseek(file, 16*(swapFrame),SEEK_SET);
     for (int x=frame*16; x<(frame+1)*16; x++){
         fwrite(&memory[x],1,1,file);
-//        printf("wrote: %d\n",memory[x]);
         memory[x]=0;
     }   
     fclose(file);
@@ -101,15 +123,13 @@ void swapclean(int frame, int swapFrame){
 int swapout(int pid){
     printf("Swapping out pid: %d\n", pid);
     for (int VPN=0; VPN<4; VPN++){
-            printf("reading: %d\n",pidPageTables[pid]+VPN);
-            printf("val: %d\n",memory[pidPageTables[pid]+VPN]);
         // is present
         if ((memory[pidPageTables[pid]+VPN] & 0x20)){
             int page=(memory[pidPageTables[pid]+VPN] & 0x03);
             swapclean(page, pid*5+VPN+1);
         }
     }
-    swapclean(pidPageTables[pid], pid*5);
+    swapclean(pidPageTables[pid]/16, pid*5);
     pidPageTables[pid]=-1;
 }
 
@@ -132,17 +152,16 @@ void getInput(){
 int map(int pid, int addr, int value){
     swapin(pid,addr>>4);
     int VPN = addr >> 4; // pop off the top 2
-    printf("VPN IS :%d\n",VPN);
-   // if (!memory[pidPageTables[pid] + VPN]){
-        printf("memVal: %d\n",memory[pidPageTables[pid]]);
-        //valid
-        SET_BIT(memory[pidPageTables[pid]+VPN],7);
-        if (value){// writeable
-            SET_BIT(memory[pidPageTables[pid]+VPN],6);
-        }
-        printf("memVal: %d\n",memory[pidPageTables[pid]]);
-   // }
-    sflush();
+    // valid
+    SET_BIT(memory[pidPageTables[pid]+VPN],7);
+    printf("Mapping page %d for process %d\n",VPN,pid);
+    if (value){// writeable
+        printf("Made writeable\n");
+        SET_BIT(memory[pidPageTables[pid]+VPN],6);
+    }else{
+        printf("Made not writeable\n");
+        CLEAR_BIT(memory[pidPageTables[pid]+VPN],6);
+    }
     return 1;
 }
 int store(int pid, int addr, int value){
@@ -155,14 +174,15 @@ int store(int pid, int addr, int value){
         return 0;
     }
     // test writeable
-    if (!(memory[pidPageTables[pid]+VPN] & 0x60)){
+    if (!(memory[pidPageTables[pid]+VPN] & 0x40)){
         printf("Page not writable\n");
         return 0;
     }
     int startOfPage=(memory[pidPageTables[pid]+VPN] & 0x03)*16;
-    int offset = addr & 0x3F;
+    int offset = addr & 0x0F;
+ //   printf("startof page: %d\n offset: %d\n, value: %d\n",startOfPage,offset,value);
     memory[startOfPage+offset]=value;
-    sflush();
+    printf("Storing %d at virtual address %d (physical address %d)\n",value,addr,startOfPage+offset);
     return 1;
 }
 int read(int pid, int addr, int value){
@@ -174,13 +194,12 @@ int read(int pid, int addr, int value){
         return -1;
     }
     int startOfPage=(memory[pidPageTables[pid]+VPN] & 0x03)*16;
-    int offset = addr & 0x3F;
+    int offset = addr & 0x0F;
     int val =  memory[startOfPage+offset];
-    sflush();
     return val;
 }
 int main(){
-    printf("Creating swap file");
+    printf("Creating swap file\n");
     file=fopen("swap","w");
     int zero=0;
     for (int i=0; i<20*16; i++){
@@ -193,8 +212,23 @@ int main(){
     char * pEnd;
     printf("Starting\n");
     while (1){
-        printf("Input: ");
+        printf("\nInput: ");
         getInput();
+        int in0 = atoi(input[0]);
+        int in2 = atoi(input[2]);
+        int in3 = atoi(input[3]);
+        if (in0<0 || in0>3){
+            printf("Process must be a value between 0 and 3 inclusive.\n");
+            continue;
+        }
+        if (in2<0 || in2>63){
+            printf("Storage loc must be a value between 0 and 63 inclusive\n");
+            continue;
+        }
+        if (in3<0 || in3>255){
+            printf("Value must be a value between 0 and 255 inclusive\n");
+            continue;
+        }
         if (strlen(input[1])>=3 && strncmp(input[1],"map",3)==0){
             if (map(atoi(input[0]), atoi(input[2]), atoi(input[3])))
                 printf("Mapped\n");
@@ -209,6 +243,7 @@ int main(){
         }else{
             printf("Invalid command\n");
         }
+ //       printf("Num free:%d\n",freePages());
     }
     printf("Ending\n");
 }
